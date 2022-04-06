224794731f3da1a64e50c11378813298fc09a3a8
Turan <90398677+Turan141@users.noreply.github.com>
2022-04-06 10:47:52 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="card">
+        <h1>Some Text</h1>
+        <p>Some Paragraph</p>
+        <div className="check"></div>
+      </div>
+    </div>
+  );
+}
